d3243abcad919db9ef51739d0786363829abdc64
Ravi Patel <ravipatel.it@charusat.ac.in>
2022-09-15 15:01:01 +0530

    test

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ function App() {
               >
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span class="ml-3 text-xl">Tailblocks</span>
+              <span class="ml-3 text-xl">CHARUSAT</span>
             </a>
             <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
               © 2020 Tailblocks —
